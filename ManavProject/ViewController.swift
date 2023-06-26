ea37a9a4201130b08216eba356191c38633fb73b
jigmetdadul <jigmetdadul6@gmail.com>
2023-06-26 12:25:43 +0530

    Initial Commit

---
+++ ManavProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManavProject
+//
+//  Created by Jigmet stanzin Dadul on 26/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
